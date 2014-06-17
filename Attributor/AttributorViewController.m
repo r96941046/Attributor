34cee4139e36c67b9a4d8cfd0ab1eb90b1adc4a9
Hung Yu-Ting <r96941046@gmail.com>
2014-06-17 11:38:26 +0800

    add color and outline buttons

--- Attributor/AttributorViewController.m
+++ Attributor/AttributorViewController.m
@@ -9,21 +9,32 @@
 #import "AttributorViewController.h"
 
 @interface AttributorViewController ()
+@property (weak, nonatomic) IBOutlet UITextView *body;
+@property (weak, nonatomic) IBOutlet UILabel *headline;
 
 @end
 
 @implementation AttributorViewController
 
+- (IBAction)changeBodySelectionColor:(UIButton *)sender {
+    
+    [self.body.textStorage addAttribute:NSForegroundColorAttributeName value:sender.backgroundColor range:self.body.selectedRange];
+}
+
+- (IBAction)outlineBodySelection:(UIButton *)sender {
+    
+    [self.body.textStorage addAttributes:@{ NSStrokeWidthAttributeName : @-3, NSStrokeColorAttributeName : [UIColor blackColor]} range:self.body.selectedRange];
+}
+
+- (IBAction)unOutlineBodySelection:(UIButton *)sender {
+    
+    [self.body.textStorage removeAttribute:NSStrokeWidthAttributeName range:self.body.selectedRange];
+}
+
 - (void)viewDidLoad
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
-}
-
-- (void)didReceiveMemoryWarning
-{
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
 }
 
 @end
